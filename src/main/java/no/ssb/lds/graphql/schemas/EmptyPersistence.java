e6b7fd29c373a70e147f20de56fef18b8c0b5b2e
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-03-08 13:59:56 +0100

    Extract mock persistence so we can use it from main context

---
+++ src/main/java/no/ssb/lds/graphql/schemas/EmptyPersistence.java
@@ -0,0 +1,100 @@
+package no.ssb.lds.graphql.schemas;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import no.ssb.lds.api.json.JsonNavigationPath;
+import no.ssb.lds.api.persistence.PersistenceDeletePolicy;
+import no.ssb.lds.api.persistence.PersistenceException;
+import no.ssb.lds.api.persistence.Transaction;
+import no.ssb.lds.api.persistence.TransactionStatistics;
+import no.ssb.lds.api.persistence.json.JsonDocument;
+import no.ssb.lds.api.persistence.reactivex.Range;
+import no.ssb.lds.api.persistence.reactivex.RxJsonPersistence;
+import no.ssb.lds.api.specification.Specification;
+
+import java.time.ZonedDateTime;
+import java.util.concurrent.CompletableFuture;
+
+class EmptyPersistence implements RxJsonPersistence {
+    @Override
+    public Maybe<JsonDocument> readDocument(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
+        return Maybe.empty();
+    }
+
+    @Override
+    public Flowable<JsonDocument> readDocuments(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, Range<String> range) {
+        return Flowable.empty();
+    }
+
+    @Override
+    public Flowable<JsonDocument> readDocumentVersions(Transaction tx, String ns, String entityName, String id, Range<ZonedDateTime> range) {
+        return Flowable.empty();
+    }
+
+    @Override
+    public Flowable<JsonDocument> readLinkedDocuments(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id, JsonNavigationPath jsonNavigationPath, String targetEntityName, Range<String> range) {
+        return Flowable.empty();
+    }
+
+    @Override
+    public Completable createOrOverwrite(Transaction tx, JsonDocument document, Specification specification) {
+        return Completable.complete();
+    }
+
+    @Override
+    public Completable deleteDocument(Transaction tx, String ns, String entityName, String id, ZonedDateTime version, PersistenceDeletePolicy policy) {
+        return Completable.complete();
+    }
+
+    @Override
+    public Completable deleteAllDocumentVersions(Transaction tx, String ns, String entity, String id, PersistenceDeletePolicy policy) {
+        return Completable.complete();
+    }
+
+    @Override
+    public Completable deleteAllEntities(Transaction tx, String namespace, String entity, Specification specification) {
+        return Completable.complete();
+    }
+
+    @Override
+    public Completable markDocumentDeleted(Transaction transaction, String ns, String entityName, String id, ZonedDateTime version, PersistenceDeletePolicy policy) {
+        return Completable.complete();
+    }
+
+    @Override
+    public Single<Boolean> hasPrevious(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
+        return Single.just(false);
+    }
+
+    @Override
+    public Single<Boolean> hasNext(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
+        return Single.just(false);
+    }
+
+    @Override
+    public Transaction createTransaction(boolean readOnly) throws PersistenceException {
+        return new Transaction() {
+            @Override
+            public CompletableFuture<TransactionStatistics> commit() {
+                return CompletableFuture.completedFuture(new TransactionStatistics());
+            }
+
+            @Override
+            public CompletableFuture<TransactionStatistics> cancel() {
+                return CompletableFuture.completedFuture(new TransactionStatistics());
+            }
+        };
+    }
+
+    @Override
+    public Flowable<JsonDocument> findDocument(Transaction tx, ZonedDateTime snapshot, String namespace, String entityName, JsonNavigationPath path, String value, Range<String> range) {
+        return Flowable.empty();
+    }
+
+    @Override
+    public void close() throws PersistenceException {
+
+    }
+}
--- src/test/java/no/ssb/lds/graphql/schemas/GraphqlSchemaBuilderTest.java
+++ src/test/java/no/ssb/lds/graphql/schemas/GraphqlSchemaBuilderTest.java
@@ -2,26 +2,14 @@ package no.ssb.lds.graphql.schemas;
 
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.SchemaPrinter;
-import io.reactivex.Completable;
-import io.reactivex.Flowable;
-import io.reactivex.Maybe;
-import io.reactivex.Single;
 import no.ssb.config.DynamicConfiguration;
 import no.ssb.config.StoreBasedDynamicConfiguration;
-import no.ssb.lds.api.json.JsonNavigationPath;
-import no.ssb.lds.api.persistence.PersistenceDeletePolicy;
-import no.ssb.lds.api.persistence.PersistenceException;
-import no.ssb.lds.api.persistence.Transaction;
-import no.ssb.lds.api.persistence.json.JsonDocument;
-import no.ssb.lds.api.persistence.reactivex.Range;
 import no.ssb.lds.api.persistence.reactivex.RxJsonPersistence;
 import no.ssb.lds.api.search.SearchIndex;
 import no.ssb.lds.api.specification.Specification;
 import no.ssb.lds.core.search.SearchIndexConfigurator;
 import no.ssb.lds.core.specification.JsonSchemaBasedSpecification;
 import org.testng.annotations.Test;
-
-import java.time.ZonedDateTime;
 
 public class GraphqlSchemaBuilderTest {
 
@@ -61,7 +49,7 @@ public class GraphqlSchemaBuilderTest {
                 "no/ssb/lds/graphql/schemas/Role.json"
 
         );
-        RxJsonPersistence fakePersistence = new MockPersistence();
+        RxJsonPersistence fakePersistence = new EmptyPersistence();
         DynamicConfiguration configuration = new StoreBasedDynamicConfiguration.Builder()
                 .values("graphql.search.enabled", "true")
                 .values("search.index.provider", "testSearchIndex")
@@ -76,75 +64,4 @@ public class GraphqlSchemaBuilderTest {
 
     }
 
-    private class MockPersistence implements RxJsonPersistence {
-        @Override
-        public Maybe<JsonDocument> readDocument(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
-            return null;
-        }
-
-        @Override
-        public Flowable<JsonDocument> readDocuments(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, Range<String> range) {
-            return null;
-        }
-
-        @Override
-        public Flowable<JsonDocument> readDocumentVersions(Transaction tx, String ns, String entityName, String id, Range<ZonedDateTime> range) {
-            return null;
-        }
-
-        @Override
-        public Flowable<JsonDocument> readLinkedDocuments(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id, JsonNavigationPath jsonNavigationPath, String targetEntityName, Range<String> range) {
-            return null;
-        }
-
-        @Override
-        public Completable createOrOverwrite(Transaction tx, JsonDocument document, Specification specification) {
-            return null;
-        }
-
-        @Override
-        public Completable deleteDocument(Transaction tx, String ns, String entityName, String id, ZonedDateTime version, PersistenceDeletePolicy policy) {
-            return null;
-        }
-
-        @Override
-        public Completable deleteAllDocumentVersions(Transaction tx, String ns, String entity, String id, PersistenceDeletePolicy policy) {
-            return null;
-        }
-
-        @Override
-        public Completable deleteAllEntities(Transaction tx, String namespace, String entity, Specification specification) {
-            return null;
-        }
-
-        @Override
-        public Completable markDocumentDeleted(Transaction transaction, String ns, String entityName, String id, ZonedDateTime version, PersistenceDeletePolicy policy) {
-            return null;
-        }
-
-        @Override
-        public Single<Boolean> hasPrevious(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
-            return null;
-        }
-
-        @Override
-        public Single<Boolean> hasNext(Transaction tx, ZonedDateTime snapshot, String ns, String entityName, String id) {
-            return null;
-        }
-
-        @Override
-        public Transaction createTransaction(boolean readOnly) throws PersistenceException {
-            return null;
-        }
-
-        @Override
-        public Flowable<JsonDocument> findDocument(Transaction tx, ZonedDateTime snapshot, String namespace, String entityName, JsonNavigationPath path, String value, Range<String> range) {
-            return null;
-        }
-
-        @Override
-        public void close() throws PersistenceException {
-
-        }
-    }
 }
