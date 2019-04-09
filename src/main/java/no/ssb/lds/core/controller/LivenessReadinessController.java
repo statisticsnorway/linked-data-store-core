a22dc997d512618c520c5364e896bfff0db97da1
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-04-09 15:44:25 +0200

    Correct health handler after review

--- src/main/java/no/ssb/lds/core/controller/LivenessReadinessController.java
+++ src/main/java/no/ssb/lds/core/controller/LivenessReadinessController.java
@@ -3,14 +3,23 @@ package no.ssb.lds.core.controller;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import no.ssb.lds.api.persistence.PersistenceException;
-import no.ssb.lds.api.persistence.reactivex.RxJsonPersistence;
 import no.ssb.lds.api.persistence.Transaction;
+import no.ssb.lds.api.persistence.reactivex.RxJsonPersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.HttpURLConnection;
 
+/**
+ * Handler that provides liveliness and health check.
+ */
 class LivenessReadinessController implements HttpHandler {
 
-    final RxJsonPersistence persistence;
+    public static final String HEALTH_PATH = "/health";
+    public static final String HEALTH_ALIVE_PATH = HEALTH_PATH + "/alive";
+    public static final String HEALTH_READY_PATH = HEALTH_PATH + "/ready";
+    private static final Logger log = LoggerFactory.getLogger(LivenessReadinessController.class);
+    private final RxJsonPersistence persistence;
 
     public LivenessReadinessController(RxJsonPersistence persistence) {
         this.persistence = persistence;
@@ -18,20 +27,31 @@ class LivenessReadinessController implements HttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) {
-
-        if (exchange.getRequestPath().startsWith("/health/alive")) {
+        if (exchange.getRequestPath().startsWith(HEALTH_ALIVE_PATH)) {
             exchange.setStatusCode(HttpURLConnection.HTTP_OK);
+        } else if (exchange.getRequestPath().startsWith(HEALTH_READY_PATH)) {
 
-        } else if (exchange.getRequestPath().startsWith("/health/ready")) {
+            // Dispatch here since we are calling persistence.
+            if (exchange.isInIoThread()) {
+                exchange.dispatch(this);
+                return;
+            }
 
             try {
+                // TODO: Extract to health() method in RxJsonPersistence interface.
                 Transaction transaction = persistence.createTransaction(true);
                 transaction.cancel();
                 exchange.setStatusCode(HttpURLConnection.HTTP_OK);
-
             } catch (PersistenceException e) {
                 exchange.setStatusCode(HttpURLConnection.HTTP_UNAVAILABLE);
-
+                if (log.isDebugEnabled()) {
+                    log.debug("health check failed", e);
+                } else {
+                    log.warn("health check failed: {}", e.getMessage());
+                }
+            } catch (Exception e) {
+                exchange.setStatusCode(HttpURLConnection.HTTP_UNAVAILABLE);
+                log.warn("unexpected exception during health check {}", persistence, e);
             }
         }
     }
