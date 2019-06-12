1e091e977d0f99831d3d99a1427eba2ecb74e2f4
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-06-12 15:48:08 +0200

    Restore unit-test.

--- src/test/java/no/ssb/lds/core/domain/managed/ManagedTimeBasedVersioningTest.java
+++ src/test/java/no/ssb/lds/core/domain/managed/ManagedTimeBasedVersioningTest.java
@@ -18,14 +18,14 @@ public class ManagedTimeBasedVersioningTest {
     @Inject
     private TestServer server;
 
-    @Test(enabled = false)
+    @Test
     public void thatCorrectVersionIsReturned() {
         String body2016 = "{\"name\":\"version2016\"}";
         String body2017 = "{\"name\":\"version2017\"}";
         String body2018 = "{\"name\":\"version2018\"}";
-        client.put("/data/provisionagreement/A1?sync=true&timestamp=2016-01-01T00%3A00%3A00.000%2B01", body2016).expect200Ok();
-        client.put("/data/provisionagreement/A1?sync=true&timestamp=2017-01-01T00%3A00%3A00.000%2B01", body2017).expect200Ok();
-        client.put("/data/provisionagreement/A1?sync=true&timestamp=2018-01-01T00%3A00%3A00.000%2B01", body2018).expect200Ok();
+        client.put("/data/provisionagreement/A1?sync=true&timestamp=2016-01-01T00%3A00%3A00.000%2B01", body2016).expect201Created();
+        client.put("/data/provisionagreement/A1?sync=true&timestamp=2017-01-01T00%3A00%3A00.000%2B01", body2017).expect201Created();
+        client.put("/data/provisionagreement/A1?sync=true&timestamp=2018-01-01T00%3A00%3A00.000%2B01", body2018).expect201Created();
         client.get("/data/provisionagreement/A1?timestamp=2015-07-01T00%3A00%3A00.000%2B01").expect404NotFound();
         JSONAssert.assertEquals(client.get("/data/provisionagreement/A1?timestamp=2016-07-01T00%3A00%3A00.000%2B01").expect200Ok().body(), body2016, false);
         JSONAssert.assertEquals(client.get("/data/provisionagreement/A1?timestamp=2017-07-01T00%3A00%3A00.000%2B01").expect200Ok().body(), body2017, false);
