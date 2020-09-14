8d06eb0b5028532fa1d19f12c5580d5277d53182
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-09-14 12:30:16 +0200

    Added limit to cypher-query test

--- src/test/java/no/ssb/lds/graphqlneo4j/CypherQueryTransformerTest.java
+++ src/test/java/no/ssb/lds/graphqlneo4j/CypherQueryTransformerTest.java
@@ -22,7 +22,7 @@ public class CypherQueryTransformerTest {
                     ]
                     WHERE instanceVariable_RepresentedVariable_Cond
                   )
-                RETURN instanceVariable""";
+                RETURN instanceVariable LIMIT 10""";
         String transformedCypher = new CypherQueryTransformer(Set.of("InstanceVariable", "RepresentedVariable")).transform(cypherQuery);
         System.out.printf("%s%n", transformedCypher);
     }
@@ -31,7 +31,7 @@ public class CypherQueryTransformerTest {
     public void gsim2() {
         String cypherQuery = """
                 MATCH (instanceVariable:InstanceVariable) WHERE ALL(instanceVariable_RepresentedVariable_Cond IN [(instanceVariable)-[:representedVariable]->(instanceVariable_RepresentedVariable) | ( ALL(instanceVariable_RepresentedVariable_MultilingualText_Cond IN [(instanceVariable_RepresentedVariable)-[:description]->(instanceVariable_RepresentedVariable_MultilingualText) | (instanceVariable_RepresentedVariable_MultilingualText.languageText CONTAINS $filterInstanceVariable_RepresentedVariable_MultilingualTextLanguageText_C)] WHERE instanceVariable_RepresentedVariable_MultilingualText_Cond))] WHERE instanceVariable_RepresentedVariable_Cond)
-                RETURN instanceVariable { .id, .shortName, representedVariable:[instanceVariableRepresentedVariable IN apoc.cypher.runFirstColumnSingle('WITH $this AS this, $ver AS ver MATCH (this)-[:representedVariable]->(:RESOURCE)<-[v:VERSION_OF]-(n) WHERE v.from <= ver AND coalesce(ver < v.to, true) RETURN n', { this:instanceVariable, ver:$_version }) | instanceVariableRepresentedVariable { name:[(instanceVariableRepresentedVariable)-[:name]->(instanceVariableRepresentedVariableName:MultilingualText) | instanceVariableRepresentedVariableName { .languageText }] }][0] } AS instanceVariable
+                RETURN instanceVariable { .id, .shortName, representedVariable:[instanceVariableRepresentedVariable IN apoc.cypher.runFirstColumnSingle('WITH $this AS this, $ver AS ver MATCH (this)-[:representedVariable]->(:RESOURCE)<-[v:VERSION_OF]-(n) WHERE v.from <= ver AND coalesce(ver < v.to, true) RETURN n', { this:instanceVariable, ver:$_version }) | instanceVariableRepresentedVariable { name:[(instanceVariableRepresentedVariable)-[:name]->(instanceVariableRepresentedVariableName:MultilingualText) | instanceVariableRepresentedVariableName { .languageText }] }][0] } AS instanceVariable LIMIT 10
                 """;
         String transformedCypher = new CypherQueryTransformer(Set.of("InstanceVariable", "RepresentedVariable")).transform(cypherQuery);
         System.out.printf("%s%n", transformedCypher);
